eff4fcf73d4d00564e9bde6bd79234350dd86ace
amirreza788790 <138209525+amirreza788790@users.noreply.github.com>
2023-07-12 13:42:06 -0700

    Add files via upload

---
+++ filter_ghoghnos.py
@@ -0,0 +1,106 @@
+#test skript filter_ghoghnos
+#GHOGHNOS_BLACK
+import random
+#start
+b = random.randint(100,900)
+
+print(f'\033[92m')
+
+for i in range(5):
+    a=input("""" 
+            >>> PING_HASAS    = 1
+            >>> CODE_HASAS    = 2
+            >>> ADAD_MOKHAREB = 3
+            >>> LINK_SEX      = 4
+            <<>>
+            >>>>>>>>>>>>>>>> """)
+
+
+    if a == '1':
+        print(f'\033[93m')
+        print('''
+5.1.1.2
+2.4.0.1
+4.0.7.5
+7.8.9.2
+5.6.8.3
+3.9.2.3
+6.0.3.2
+0.3.4.0
+7.0.2.5
+3.8.6.4
+2.3.0.4
+              
+                      GHOGHNOS_BLACK
+
+              ''')
+    
+
+    elif a == '2':
+        print(f'\033[94m')
+        print(''' 
+y//d/f
+y//d/
+g/d//
+d//f/h
+f//a/y
+f///g.h
+/e/f///g.h////
+/f/g.h////
+///g///h
+e////f.g/
+e////fg/
+f////gh/
+f////g.h/
+              
+                   GHOGHNOS_BLACK
+              
+              ''')
+    
+
+    elif a == '3':
+        print(f'\033[95m')
+        print('        ',b)
+    
+
+    elif a == '4':
+        print(f'\033[91m')
+        print('''
+<img src="https://uupload.ir/view/vid_20230414_020856_166_lvgd.mp4/">
+<img src="https://uupload.ir/view/vid_20230414_020933_568_7v8p.mp4/">
+<img src="https://uupload.ir/view/vid_20230414_020934_698_6clx.mp4/">
+<img src="https://uupload.ir/view/vid_20230414_020946_356_xv5s.mp4/">
+<img src="https://uupload.ir/view/vid_20230414_020856_166_lvgd.mp4/">
+<img src="https://uupload.ir/view/vid_20230414_020933_568_7v8p.mp4/">
+<img src="https://uupload.ir/view/vid_20230414_020934_698_6clx.mp4/">
+<img src="https://uupload.ir/view/vid_20230414_020946_356_xv5s.mp4/">
+<img src="https://uupload.ir/view/vid_20230414_020856_166_lvgd.mp4/">
+<img src="https://uupload.ir/view/vid_20230414_020933_568_7v8p.mp4/">
+<img src="https://uupload.ir/view/vid_20230414_020934_698_6clx.mp4/">
+<img src="https://uupload.ir/view/vid_20230414_020946_356_xv5s.mp4/">
+<img src="https://uupload.ir/view/vid_20230414_020856_166_lvgd.mp4/">
+<img src="https://uupload.ir/view/vid_20230414_020933_568_7v8p.mp4/">
+<img src="https://uupload.ir/view/vid_20230414_020934_698_6clx.mp4/">
+<img src="https://uupload.ir/view/vid_20230414_020946_356_xv5s.mp4/">
+<img src="https://uupload.ir/view/vid_20230414_020856_166_lvgd.mp4/">
+<img src="https://uupload.ir/view/vid_20230414_020933_568_7v8p.mp4/">
+<img src="https://uupload.ir/view/vid_20230414_020934_698_6clx.mp4/">
+<img src="https://uupload.ir/view/vid_20230414_020946_356_xv5s.mp4/">
+<img src="https://uupload.ir/view/vid_20230414_020856_166_lvgd.mp4/">
+<img src="https://uupload.ir/view/vid_20230414_020933_568_7v8p.mp4/">
+<img src="https://uupload.ir/view/vid_20230414_020934_698_6clx.mp4/">
+<img src="https://uupload.ir/view/vid_20230414_020946_356_xv5s.mp4/">
+<img src="https://uupload.ir/view/vid_20230414_020856_166_lvgd.mp4/">
+<img src="https://uupload.ir/view/vid_20230414_020933_568_7v8p.mp4/">
+<img src="https://uupload.ir/view/vid_20230414_020934_698_6clx.mp4/">
+<img src="https://uupload.ir/view/vid_20230414_020946_356_xv5s.mp4/">
+              
+              
+                                 GHOGHNOS_BLACK               
+               ''')
+    
+
+
+    else:
+        print(f'\033[97m')
+        print(" ERROR 1,2,3,4 PLIS")
